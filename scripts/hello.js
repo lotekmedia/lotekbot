83f19a68a066194715102c847b6db72943469c9c
jmann-ts <john@travelspike.com>
2016-04-10 09:16:36 -0400

    listen

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,10 +1,20 @@
 module.exports = function(robot) {
-    robot.respond(/is it (weekend|holiday)\s?\?/i, function(msg){
+    robot.respond(/is it a (weekend|holiday)\s?\?/i, function(msg){
         var today = new Date();
 
         msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
     });
+    
     robot.hear(/badger/i, function(msg){
         msg.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS");
     });
+    
+    robot.hear(/i did it/i, function(msg){
+        msg.send("Congratulations! Good job!");
+    });
+    
+    robot.respond(/are you there?/i, function(msg){
+        msg.reply('Yes, still here, and listening.');
+    })
+
 }
